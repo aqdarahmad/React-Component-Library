f585678d8546ec6cd88eac59e9feb58e94f3c1cb
aqdarahmad <a.abusufyan@student.aaup.edu>
2025-08-19 10:58:33 +0300

    sum/avg/inc/dec  operations

--- src/basic.ts
+++ src/basic.ts
@@ -5,3 +5,14 @@ export const div = (a: number, b: number) => {
   if (b === 0) throw new Error("Cant divide");
   return a / b;
 };
+export const mod = (a: number, b: number) => a % b;
+export const pow = (a: number, b: number) => Math.pow(a, b);
+export const factorial = (n: number): number => {
+  if (n < 0) throw new Error("Factorial not defined for negative numbers");
+  return n <= 1 ? 1 : n * factorial(n - 1);
+};
+export const sum = (...nums: number[]) => nums.reduce((acc, val) => acc + val, 0);
+export const average = (...nums: number[]) => sum(...nums) / nums.length;
+export const negate = (x: number) => -x;
+export const increment = (x: number) => x + 1;
+export const decrement = (x: number) => x - 1
